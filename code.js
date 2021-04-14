cf729f4e91fe1f3c125ab68e61aced7be26c668d
sonyarybska <sofaribska@gmail.com>
2021-04-14 19:39:52 +0300

    Initial commit

---
+++ code.js
@@ -0,0 +1,48 @@
+
+//Створити три чекбокси. Кожний з них активує фільтр для вищевказаного масиву. Фільтри можуть працювати як разом так і окремо.
+// 1й - відфільтровує юзерів зі статусом false (залишає зі статусом false)
+// 2й - залишає старших 29 років включно
+// 3й - залишає тих в кого місто Київ
+// Дані виводить в документ
+/*
+
+const usersWithAddress = [
+    {id: 9, name: 'vasya', age: 31, isMarried: false, address: {city: 'Kyiv', street: 'Gongadze', number: 16}},
+    {id: 2, name: 'petya', age: 30, isMarried: true, address: {city: 'Rivne', street: 'Zelena', number: 1}},
+    {id: 4, name: 'kolya', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Pasichna', number: 121}},
+    {id: 3, name: 'olya', age: 28, isMarried: false, address: {city: 'Rivne', street: 'Shevchenko', number: 90}},
+    {id: 8, name: 'max', age: 30, isMarried: true, address: {city: 'Lviv', street: 'Kriva Lipa', number: 115}},
+    {id: 6, name: 'anya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}},
+    {id: 10, name: 'oleg', age: 28, isMarried: false, address: {city: 'Kyiv', street: 'Centralna', number: 22}},
+    {id: 5, name: 'andrey', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Gorodotska', number: 43}},
+    {id: 1, name: 'masha', age: 30, isMarried: true, address: {city: 'Kyiv', street: 'Peremogi', number: 12}},
+    {id: 7, name: 'olya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Naukova', number: 16}},
+    {id: 11, name: 'max', age: 31, isMarried: true, address: {city: 'Rivne', street: 'Ivana Franka', number: 121}}
+];
+let a;
+let box1=document.querySelector('#status');
+let box2=document.querySelector('#age');
+let box3=document.querySelector('#city ');
+let divka=document.querySelector('#divka');
+document.body.appendChild(divka);
+let myArray=JSON.parse(JSON.stringify(usersWithAddress));
+
+box1.onclick=()=>{
+    if (box1.checked) myArray=myArray.filter(value=>value.isMarried===false);
+    let string=JSON.stringify(myArray);
+    divka.innerHTML=string;
+
+}
+box2.onclick=()=> {
+    if (box2.checked) myArray=myArray.filter(value => value.age >= 29);
+
+    let string=JSON.stringify(myArray);
+    divka.innerHTML=string;
+}
+
+box3.onclick=()=> {
+    if (box3.checked) myArray =myArray.filter(value => value.address.city === 'Kyiv');
+    let string=JSON.stringify(myArray);
+    divka.innerHTML=string;
+}
+*/
